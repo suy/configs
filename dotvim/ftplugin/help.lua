85e35fb5288a193955fdc7a53287d75d84ed8620
Alejandro Exojo <suy@badopi.org>
2024-11-30 19:43:36 +0100

    Better positioning of help window

--- dotvim/ftplugin/help.lua
+++ dotvim/ftplugin/help.lua
@@ -1,8 +1,12 @@
+-- Move window to the right, as there is no setting to do it from the start.
+vim.cmd.wincmd 'L'
+
 -- No need for this on help files :)
 vim.b.miniindentscope_disable = true
+
 -- Load the helpful plugin "on demand". Set the variable so it automatically
 -- triggers on CursorMoved, enable the plugin, the set it up manually (because
 -- its set up on the FileType event, which is when this is running).
 vim.b.helpful = 1
-vim.cmd.packadd "helpful"
+vim.cmd.packadd 'helpful'
 vim.fn['helpful#setup']()
